354fc7648438435989bc75a2a307bf8a1be64089
Emanuel de Jong <44051853+Emanuel-de-Jong@users.noreply.github.com>
2025-02-10 18:21:18 +0100

    .

--- MapObjects.py
+++ MapObjects.py
@@ -3,9 +3,16 @@ from valvevmf import Vmf
 class MapObjects:
     def __init__(self, map_name):
         self.vmf = Vmf(f"css_server/server/cstrike/maps/surf_{map_name}.vmf")
-    
+
+        self.solids = []
+        for node in self.vmf.nodes:
+            if node.name == "world" or node.name == "entity":
+                for subnode in node.nodes:
+                    if subnode.name == "solid":
+                        self.solids.append(subnode)
+
     def get_near_objects(self, coord):
         pass
 
-    def get_near_ramps(self, coord):
-        pass
+if __name__ == '__main__':
+    MapObjects("beginner")
