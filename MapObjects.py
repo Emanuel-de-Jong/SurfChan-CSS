621848d3ab75fb21e5adb31585028067cd2e1dda
Emanuel de Jong <44051853+Emanuel-de-Jong@users.noreply.github.com>
2025-02-11 15:51:40 +0100

    VMF collision filter WIP

--- MapObjects.py
+++ MapObjects.py
@@ -21,18 +21,7 @@ class MapObjects:
                     pickle.dump(vmf, file)
         
         # Maybe save solids instead of vmf
-        self.solids = []
-        solid_centroids = []
-        for node in vmf.nodes:
-            if node.name in ["world", "entity"]:
-                for subnode in node.nodes:
-                    if subnode.name == "solid":
-                        centroid = self._calculate_solid_centroid(subnode)
-                        if centroid is None:
-                            continue
-
-                        self.solids.append(subnode)
-                        solid_centroids.append(centroid)
+        solid_centroids = self._filter_vmf(vmf)
         
         tree_cache_path = f"map_cache/surf_{map_name}_tree.pkl"
         if os.path.exists(tree_cache_path):
@@ -43,6 +32,49 @@ class MapObjects:
             if SHOULD_CACHE_TREE and self.obj_tree is not None:
                 with open(tree_cache_path, "wb") as file:
                     pickle.dump(self.obj_tree, file)
+    
+    def _filter_vmf(self, vmf):
+        self.solids = []
+        solid_centroids = []
+        for node in vmf.nodes:
+            if node.name in ["world", "entity"]:
+                classname = None
+                if node.name == "entity":
+                    for prop, val in node.properties:
+                        if prop == "classname":
+                            classname = val.lower().strip()
+                            break
+                
+                for subnode in node.nodes:
+                    if subnode.name == "solid":
+                        if not self._has_collision(node.name, classname):
+                            continue
+
+                        centroid = self._calculate_solid_centroid(subnode)
+                        if centroid is None:
+                            continue
+                        
+                        self.solids.append(subnode)
+                        solid_centroids.append(centroid)
+        
+        return solid_centroids
+
+    def _has_collision(self, node_type, classname):
+        if node_type == "world":
+            return True
+
+        if not classname:
+            return False
+        
+        if classname.startswith("trigger_"):
+            return False
+        if "illusionary" in classname:
+            return False
+        
+        if classname in ["func_detail", "func_brush", "func_wall", "func_physbox"]:
+            return True
+        
+        return False
 
     def _create_obj_tree(self, solid_centroids):
         self.obj_tree = None
@@ -62,13 +94,8 @@ class MapObjects:
         
         points = []
         for plane in planes:
-            for i in range(1, len(plane)):
-                for point in plane[i]:
-                    converted_point = []
-                    for num in point:
-                        converted_point.append(float(num))
-                
-                    points.append(converted_point)
+            for vertex in plane[1]:
+                points.append([float(coord) for coord in vertex])
 
         if len(points) < 3:
             return None
@@ -86,12 +113,15 @@ class MapObjects:
     def get_near_objects(self, coord, k=5, radius=None):
         coord = np.array(coord)
 
-        if radius:
+        if radius is not None:
             # Find all solids within the radius
             indices = self.obj_tree.query_ball_point(coord, r=radius)
         else:
             # Find the k-nearest solids
             _, indices = self.obj_tree.query(coord, k=k)
+
+        if isinstance(indices, int):
+            indices = [indices]
 
         return [self.solids[i] for i in indices]
 
