83f9de60040b15e4bdcb2a11d0af5d4da8892e0f
Emanuel de Jong <44051853+Emanuel-de-Jong@users.noreply.github.com>
2025-02-17 15:25:12 +0100

    .

--- src/SCTrain.py
+++ src/SCTrain.py
@@ -1,8 +1,194 @@
-
+from collections import defaultdict
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+import torch
+from torch import nn
+from tensordict.nn import TensorDictModule, set_composite_lp_aggregate
+from tensordict.nn.distributions import NormalParamExtractor
+from torchrl.collectors import SyncDataCollector
+from torchrl.data.replay_buffers import ReplayBuffer
+from torchrl.data.replay_buffers.samplers import SamplerWithoutReplacement
+from torchrl.data.replay_buffers.storages import LazyTensorStorage
+from torchrl.envs import Compose, DoubleToFloat, ObservationNorm, StepCounter, TransformedEnv
+from torchrl.envs.libs.gym import GymEnv
+from torchrl.envs.utils import ExplorationType, set_exploration_type
+from torchrl.modules import ProbabilisticActor, TanhNormal, ValueOperator
+from torchrl.objectives import ClipPPOLoss
+from torchrl.objectives.value import GAE
 
 class SCTrain():
     def __init__(self):
-        pass
+        set_composite_lp_aggregate(True)
+
+        device = torch.device(0) if torch.cuda.is_available() else torch.device("cpu")
+        nodes_per_layer = 256
+        lr = 0.0003
+        max_grad_norm = 1.0
+
+        frames_per_batch = 1000
+        total_frames = frames_per_batch * 10
+
+        sub_batch_size = 64
+        num_epochs = 10
+        clip_epsilon = 0.2
+        gamma = 0.99
+        lmbda = 0.95
+        entropy_eps =  	0.0001
+
+        base_env = GymEnv("InvertedDoublePendulum-v4", device=device)
+
+        env = TransformedEnv(
+            base_env,
+            Compose(
+                ObservationNorm(in_keys=["observation"]),
+                DoubleToFloat(),
+                StepCounter(),
+            ),
+        )
+
+        env.transform[0].init_stats(num_iter=frames_per_batch, reduce_dim=0, cat_dim=0)
+
+        actor_net = nn.Sequential(
+            nn.LazyLinear(nodes_per_layer, device=device),
+            nn.Tanh(),
+            nn.LazyLinear(nodes_per_layer, device=device),
+            nn.Tanh(),
+            nn.LazyLinear(nodes_per_layer, device=device),
+            nn.Tanh(),
+            nn.LazyLinear(2 * env.action_spec.shape[-1], device=device),
+            NormalParamExtractor(),
+        )
+
+        policy_module = TensorDictModule(
+            actor_net, in_keys=["observation"], out_keys=["loc", "scale"]
+        )
+
+        policy_module = ProbabilisticActor(
+            module=policy_module,
+            spec=env.action_spec,
+            in_keys=["loc", "scale"],
+            distribution_class=TanhNormal,
+            distribution_kwargs={
+                "low": env.action_spec_unbatched.space.low,
+                "high": env.action_spec_unbatched.space.high,
+            },
+            return_log_prob=True,
+        )
+
+        value_net = nn.Sequential(
+            nn.LazyLinear(nodes_per_layer, device=device),
+            nn.Tanh(),
+            nn.LazyLinear(nodes_per_layer, device=device),
+            nn.Tanh(),
+            nn.LazyLinear(nodes_per_layer, device=device),
+            nn.Tanh(),
+            nn.LazyLinear(1, device=device),
+        )
+
+        value_module = ValueOperator(
+            module=value_net,
+            in_keys=["observation"],
+        )
+
+        dummy_tensordict = env.reset()
+        value_module(dummy_tensordict)
+
+        collector = SyncDataCollector(
+            env,
+            policy_module,
+            frames_per_batch=frames_per_batch,
+            total_frames=total_frames,
+            split_trajs=False,
+            device=device,
+        )
+
+        replay_buffer = ReplayBuffer(
+            storage=LazyTensorStorage(max_size=frames_per_batch),
+            sampler=SamplerWithoutReplacement(),
+        )
+
+        advantage_module = GAE(
+            gamma=gamma, lmbda=lmbda, value_network=value_module, average_gae=True
+        )
+
+        loss_module = ClipPPOLoss(
+            actor_network=policy_module,
+            critic_network=value_module,
+            clip_epsilon=clip_epsilon,
+            entropy_bonus=bool(entropy_eps),
+            entropy_coef=entropy_eps,
+            critic_coef=1.0,
+            loss_critic_type="smooth_l1",
+        )
+
+        optim = torch.optim.Adam(loss_module.parameters(), lr)
+        scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
+            optim, total_frames // frames_per_batch, 0.0
+        )
+
+        logs = defaultdict(list)
+        pbar = tqdm(total=total_frames)
+        eval_str = ""
+
+        for i, tensordict_data in enumerate(collector):
+            for _ in range(num_epochs):
+                advantage_module(tensordict_data)
+                data_view = tensordict_data.reshape(-1)
+                replay_buffer.extend(data_view.cpu())
+                for _ in range(frames_per_batch // sub_batch_size):
+                    subdata = replay_buffer.sample(sub_batch_size)
+                    loss_vals = loss_module(subdata.to(device))
+                    loss_value = (
+                        loss_vals["loss_objective"]
+                        + loss_vals["loss_critic"]
+                        + loss_vals["loss_entropy"]
+                    )
+
+                    loss_value.backward()
+                    torch.nn.utils.clip_grad_norm_(loss_module.parameters(), max_grad_norm)
+                    optim.step()
+                    optim.zero_grad()
+
+            logs["reward"].append(tensordict_data["next", "reward"].mean().item())
+            pbar.update(tensordict_data.numel())
+            cum_reward_str = (
+                f"average reward={logs['reward'][-1]: 4.4f} (init={logs['reward'][0]: 4.4f})"
+            )
+            logs["step_count"].append(tensordict_data["step_count"].max().item())
+            stepcount_str = f"step count (max): {logs['step_count'][-1]}"
+            logs["lr"].append(optim.param_groups[0]["lr"])
+            lr_str = f"lr policy: {logs['lr'][-1]: 4.4f}"
+            if i % 10 == 0:
+                with set_exploration_type(ExplorationType.DETERMINISTIC), torch.no_grad():
+                    eval_rollout = env.rollout(1000, policy_module)
+                    logs["eval reward"].append(eval_rollout["next", "reward"].mean().item())
+                    logs["eval reward (sum)"].append(
+                        eval_rollout["next", "reward"].sum().item()
+                    )
+                    logs["eval step_count"].append(eval_rollout["step_count"].max().item())
+                    eval_str = (
+                        f"eval cumulative reward: {logs['eval reward (sum)'][-1]: 4.4f} "
+                        f"(init: {logs['eval reward (sum)'][0]: 4.4f}), "
+                        f"eval step-count: {logs['eval step_count'][-1]}"
+                    )
+                    del eval_rollout
+            pbar.set_description(", ".join([eval_str, cum_reward_str, stepcount_str, lr_str]))
+            scheduler.step()
+
+        plt.figure(figsize=(10, 10))
+        plt.subplot(2, 2, 1)
+        plt.plot(logs["reward"])
+        plt.title("training rewards (average)")
+        plt.subplot(2, 2, 2)
+        plt.plot(logs["step_count"])
+        plt.title("Max step count (training)")
+        plt.subplot(2, 2, 3)
+        plt.plot(logs["eval reward (sum)"])
+        plt.title("Return (test)")
+        plt.subplot(2, 2, 4)
+        plt.plot(logs["eval step_count"])
+        plt.title("Max step count (test)")
+        plt.show()
 
 if __name__ == "__main__":
     sc_train = SCTrain()
