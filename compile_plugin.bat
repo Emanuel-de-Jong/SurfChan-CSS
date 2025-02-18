230e0959943676d3b7ff7332228f0615f8456f0d
Emanuel de Jong <44051853+Emanuel-de-Jong@users.noreply.github.com>
2025-02-18 16:53:34 +0100

    .

--- compile_plugin.bat
+++ compile_plugin.bat
@@ -1,8 +1,4 @@
 @echo off
 
-:: Compile the SourceMod plugin
 start /wait css_server/server/cstrike/addons/sourcemod/scripting/spcomp.exe src/sourcemod_plugin.sp
 move /y sourcemod_plugin.smx css_server/server/cstrike/addons/sourcemod/plugins/surfchan.smx
-
-:: Start
-start python src/SurfChan.py
---
+++ play.bat
@@ -0,0 +1,5 @@
+@echo off
+
+call compile_plugin.bat
+
+python src/SurfChan.py
