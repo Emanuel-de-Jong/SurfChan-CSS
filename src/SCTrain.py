ec27954015774338e60ff4d590d2829a6466880d
Emanuel de Jong <44051853+Emanuel-de-Jong@users.noreply.github.com>
2025-02-20 15:22:00 +0100

    Resume fixes

--- src/SCTrain.py
+++ src/SCTrain.py
@@ -5,28 +5,15 @@ from datetime import datetime
 import tqdm
 import torch
 from tensordict import TensorDict
-from tensordict.nn import TensorDictModule
 from torchrl.collectors import SyncDataCollector
 from torchrl.data import LazyTensorStorage, TensorDictReplayBuffer
-from torchrl.data.tensor_specs import Bounded, Composite
 from torchrl.data.replay_buffers.samplers import SamplerWithoutReplacement
-from torchrl.envs.utils import ExplorationType, set_exploration_type
-from torchrl.modules import (
-    ProbabilisticActor,
-    TanhNormal,
-    ValueOperator,
-    ConvNet,
-    MLP,
-    ActorValueOperator,
-    NormalParamExtractor
-)
-from torchrl.objectives import ClipPPOLoss
 from torchrl.objectives.value import GAE
 from torchrl.record.loggers.tensorboard import TensorboardLogger
 from torchrl._utils import compile_with_warmup, timeit
 from sc_config import get_config, CONFIG_FILE_NAME
 from sc_utils import get_torch_device
-from sc_model_utils import load_latest_models
+from sc_model_utils import get_models
 from SCEnv import create_torchrl_env
 
 class SCTrain():
@@ -46,12 +33,7 @@ class SCTrain():
         
         self.env = create_torchrl_env(self.config.env.name, self.config.train.map)
         
-        actor, critic = None, None
-        if self.config.train.should_resume:
-            actor, critic = load_latest_models()
-        
-        if actor is None or critic is None:
-            actor, critic = self.make_models()
+        actor, critic, loss_module, optim = get_models(self.env, self.device)
 
         collector = SyncDataCollector(
             create_env_fn=self.env,
@@ -80,22 +62,6 @@ class SCTrain():
             average_gae=False,
             device=self.device,
             vectorized=not should_compile,
-        )
-        loss_module = ClipPPOLoss(
-            actor_network=actor,
-            critic_network=critic,
-            clip_epsilon=self.config.train.loss.clip_epsilon,
-            loss_critic_type=self.config.train.loss.loss_critic_type,
-            entropy_coef=self.config.train.loss.entropy_coef,
-            critic_coef=self.config.train.loss.critic_coef,
-            normalize_advantage=True,
-        )
-
-        optim = torch.optim.Adam(
-            loss_module.parameters(),
-            lr=self.config.train.optim.lr,
-            weight_decay=self.config.train.optim.weight_decay,
-            eps=self.config.train.optim.eps,
         )
 
         date_str = datetime.now().strftime("%d-%m_%H-%M")
@@ -220,102 +186,11 @@ class SCTrain():
         training_seconds = time.time() - training_seconds_start
         print(f"Training time: {training_seconds:.2f}s or {training_seconds/60:.2f}m or {training_seconds/3600:.2f}h")
 
-        self.save(actor, critic, date_str)
+        self.save(actor, critic, optim, date_str)
 
         collector.shutdown()
-        
-    def make_models(self):
-        input_shape = self.env.observation_spec["pixels"].shape
-        num_outputs = self.env.action_spec.shape[0]
-
-        common_cnn = ConvNet(
-            activation_class=torch.nn.ReLU,
-            num_cells=[32, 64, 64],
-            kernel_sizes=[8, 4, 3],
-            strides=[4, 2, 1],
-            device=self.device,
-        )
-        common_cnn_output = common_cnn(torch.ones(input_shape, device=self.device))
-        common_mlp = MLP(
-            in_features=common_cnn_output.shape[-1],
-            activation_class=torch.nn.ReLU,
-            activate_last_layer=True,
-            out_features=512,
-            num_cells=[],
-            device=self.device,
-        )
-        common_mlp_output = common_mlp(common_cnn_output)
-
-        common_module = TensorDictModule(
-            module=torch.nn.Sequential(common_cnn, common_mlp),
-            in_keys=["pixels"],
-            out_keys=["common_features"],
-        )
-
-        policy_net = MLP(
-            in_features=common_mlp.out_features,
-            out_features=num_outputs * 2,
-            activation_class=torch.nn.ReLU,
-            num_cells=[],
-            device=self.device,
-        )
-        policy_module = TensorDictModule(
-            module=torch.nn.Sequential(
-                policy_net,
-                NormalParamExtractor(scale_mapping="exp")
-            ),
-            in_keys=["common_features"],
-            out_keys=["loc", "scale"],
-        )
-
-        spec = Composite(
-            action=Bounded(
-                low=0.0, high=1.0, shape=(num_outputs,), dtype=torch.float32, device=self.device
-            )
-        )
-
-        policy_module = ProbabilisticActor(
-            policy_module,
-            in_keys=["loc", "scale"],
-            spec=spec,
-            distribution_class=TanhNormal,
-            distribution_kwargs={
-                "low": 0.0,
-                "high": 1.0,
-            },
-            return_log_prob=True,
-            default_interaction_type=ExplorationType.RANDOM,
-        )
-
-        value_net = MLP(
-            activation_class=torch.nn.ReLU,
-            in_features=common_mlp_output.shape[-1],
-            out_features=1,
-            num_cells=[],
-            device=self.device,
-        )
-        value_module = ValueOperator(
-            value_net,
-            in_keys=["common_features"],
-        )
-
-        actor_critic = ActorValueOperator(
-            common_operator=common_module,
-            policy_operator=policy_module,
-            value_operator=value_module,
-        )
-
-        with torch.no_grad():
-            td = self.env.fake_tensordict().expand(10)
-            actor_critic(td)
-            del td
-
-        actor = actor_critic.get_policy_operator()
-        critic = actor_critic.get_value_operator()
-
-        return actor, critic
     
-    def save(self, actor, critic, date_str):
+    def save(self, actor, critic, optim, date_str):
         results_dir = self.config.model.results_dir
         if not os.path.exists(results_dir):
             os.makedirs(results_dir)
@@ -324,6 +199,7 @@ class SCTrain():
         shutil.copy2(CONFIG_FILE_NAME, os.path.join(results_dir, f"{date_str}_config.yml"))
         torch.save(actor.state_dict(), os.path.join(results_dir, f"{date_str}_actor.pth"))
         torch.save(critic.state_dict(), os.path.join(results_dir, f"{date_str}_critic.pth"))
+        torch.save(optim.state_dict(), os.path.join(results_dir, f"{date_str}_optim.pth"))
 
     def close(self):
         pass
--- src/sc_model_utils.py
+++ src/sc_model_utils.py
@@ -1,19 +1,166 @@
 import os
+from datetime import datetime
 from sc_config import get_config
 import torch
+from tensordict.nn import TensorDictModule
+from torchrl.data.tensor_specs import Bounded, Composite
+from torchrl.envs.utils import ExplorationType
+from torchrl.objectives import ClipPPOLoss
+from torchrl.modules import (
+    ProbabilisticActor,
+    TanhNormal,
+    ValueOperator,
+    ConvNet,
+    MLP,
+    ActorValueOperator,
+    NormalParamExtractor
+)
 
 config = get_config()
-def load_latest_models():
+
+def get_models(env, device):
+    global config
+    actor, critic, loss_module, optim = None, None, None, None
+    if config.train.should_resume:
+        actor, critic, loss_module, optim = load_latest_models(env, device)
+
+    if actor is None:
+        print(f"Created new models")
+        actor, critic, loss_module, optim = create_models(env, device)
+
+    return actor, critic, loss_module, optim
+
+def load_latest_models(env, device):
     global config
     results_dir = config.model.results_dir
+    if not os.path.exists(results_dir):
+        return None, None, None, None
+    
     result_paths = [os.path.join(results_dir, p) for p in os.listdir(results_dir)]
     actor_paths = [p for p in result_paths if p.endswith('actor.pth')]
     if len(actor_paths) == 0:
-        return None, None
+        return None, None, None, None
     
     actor_path = max(actor_paths, key=lambda p: os.path.getctime(p))
     critic_path = actor_path.replace('actor', 'critic')
+    optim_path = actor_path.replace('actor', 'optim')
 
-    actor = torch.load(actor_path)
-    critic = torch.load(critic_path)
-    return actor, critic
+    actor, critic, loss_module, optim = create_models(env, device)
+
+    actor.load_state_dict(torch.load(actor_path, map_location=device))
+    critic.load_state_dict(torch.load(critic_path, map_location=device))
+    optim.load_state_dict(torch.load(optim_path, map_location=device))
+
+    models_date = datetime.fromtimestamp(os.path.getctime(actor_path)).strftime("%d-%m-%y %H:%M:%S")
+    print(f"Loaded models from {models_date}")
+
+    return actor, critic, loss_module, optim
+
+def create_models(env, device):
+    global config
+    input_shape = env.observation_spec["pixels"].shape
+    num_outputs = env.action_spec.shape[0]
+
+    common_cnn = ConvNet(
+        activation_class=torch.nn.ReLU,
+        num_cells=[32, 64, 64],
+        kernel_sizes=[8, 4, 3],
+        strides=[4, 2, 1],
+        device=device,
+    )
+    common_cnn_output = common_cnn(torch.ones(input_shape, device=device))
+    common_mlp = MLP(
+        in_features=common_cnn_output.shape[-1],
+        activation_class=torch.nn.ReLU,
+        activate_last_layer=True,
+        out_features=512,
+        num_cells=[],
+        device=device,
+    )
+    common_mlp_output = common_mlp(common_cnn_output)
+
+    common_module = TensorDictModule(
+        module=torch.nn.Sequential(common_cnn, common_mlp),
+        in_keys=["pixels"],
+        out_keys=["common_features"],
+    )
+
+    policy_net = MLP(
+        in_features=common_mlp.out_features,
+        out_features=num_outputs * 2,
+        activation_class=torch.nn.ReLU,
+        num_cells=[],
+        device=device,
+    )
+    policy_module = TensorDictModule(
+        module=torch.nn.Sequential(
+            policy_net,
+            NormalParamExtractor(scale_mapping="exp")
+        ),
+        in_keys=["common_features"],
+        out_keys=["loc", "scale"],
+    )
+
+    spec = Composite(
+        action=Bounded(
+            low=0.0, high=1.0, shape=(num_outputs,), dtype=torch.float32, device=device
+        )
+    )
+
+    policy_module = ProbabilisticActor(
+        policy_module,
+        in_keys=["loc", "scale"],
+        spec=spec,
+        distribution_class=TanhNormal,
+        distribution_kwargs={
+            "low": 0.0,
+            "high": 1.0,
+        },
+        return_log_prob=True,
+        default_interaction_type=ExplorationType.RANDOM,
+    )
+
+    value_net = MLP(
+        activation_class=torch.nn.ReLU,
+        in_features=common_mlp_output.shape[-1],
+        out_features=1,
+        num_cells=[],
+        device=device,
+    )
+    value_module = ValueOperator(
+        value_net,
+        in_keys=["common_features"],
+    )
+
+    actor_critic = ActorValueOperator(
+        common_operator=common_module,
+        policy_operator=policy_module,
+        value_operator=value_module,
+    )
+
+    with torch.no_grad():
+        td = env.fake_tensordict().expand(10)
+        actor_critic(td)
+        del td
+
+    actor = actor_critic.get_policy_operator()
+    critic = actor_critic.get_value_operator()
+
+    loss_module = ClipPPOLoss(
+        actor_network=actor,
+        critic_network=critic,
+        clip_epsilon=config.train.loss.clip_epsilon,
+        loss_critic_type=config.train.loss.loss_critic_type,
+        entropy_coef=config.train.loss.entropy_coef,
+        critic_coef=config.train.loss.critic_coef,
+        normalize_advantage=True,
+    )
+
+    optim = torch.optim.Adam(
+        loss_module.parameters(),
+        lr=config.train.optim.lr,
+        weight_decay=config.train.optim.weight_decay,
+        eps=config.train.optim.eps,
+    )
+
+    return actor, critic, loss_module, optim
