aa4a4c18331e2fc998cd176acae7348d8e46a110
Emanuel de Jong <44051853+Emanuel-de-Jong@users.noreply.github.com>
2025-02-24 17:08:59 +0100

    Back to TKinter

--- src/SCGUI.py
+++ src/SCGUI.py
@@ -1,64 +1,35 @@
-import asyncio
-import dearpygui.dearpygui as dpg
+import tkinter as tk
 
-_TOOLBAR_HEIGHT = 50
-_GRID_SIZE = 50
-_BUTTON_PADDING = 5
-_WINDOW_PADDING = 25
-_PRESSED_COLOR = (128, 128, 128)
-_RELEASED_COLOR = (255, 255, 255)
-
-def create_theme(color):
-    with dpg.theme() as theme:
-        with dpg.theme_component(dpg.mvButton):
-            dpg.add_theme_color(dpg.mvThemeCol_Button, color, category=dpg.mvThemeCat_Core)
-    return theme
-
-_PRESSED_THEME = create_theme(_PRESSED_COLOR)
-_RELEASED_THEME = create_theme(_RELEASED_COLOR)
-
-class _GUIButton:
-    def __init__(self, label, row, col):
-        self.label = label
-        self.button_id = dpg.generate_uuid()
-
-        x = _GRID_SIZE * col + _BUTTON_PADDING
-        y = _GRID_SIZE * row + _BUTTON_PADDING + _TOOLBAR_HEIGHT
-        size = _GRID_SIZE - _BUTTON_PADDING * 2
-
-        dpg.add_button(label=label, width=size, height=size, pos=(x, y), tag=self.button_id)
-        dpg.bind_item_theme(self.button_id, _RELEASED_THEME)
+class _GUIButton():
+    def __init__(self, root, label, row, col):
+        self.label = tk.Label(root, text=label, width=7, height=3)
+        self.label.grid(row=row, column=col, padx=4, pady=4)
 
     def update(self, pressed):
-        dpg.bind_item_theme(self.button_id, _PRESSED_THEME if pressed else _RELEASED_THEME)
+        self.label.config(bg="gray" if pressed else "white")
 
-class SCGUI:
+class SCGUI():
     current_buttons = set()
 
     def __init__(self):
-        window_width = _GRID_SIZE * 3 + _BUTTON_PADDING * 2
-        window_height = window_width + _TOOLBAR_HEIGHT
-        vp_width = window_width + _WINDOW_PADDING * 2
-        vp_height = window_height + _WINDOW_PADDING * 2
+        root = tk.Tk()
+        root.title("")
+        root.attributes('-topmost', True)
+        self.root = root
 
-        dpg.create_context()
-        dpg.create_viewport(title="SurfChan", width=vp_width, height=vp_height, always_on_top=True)
-        dpg.setup_dearpygui()
+        buttons = {}
+        buttons['f'] = _GUIButton(root, '↑', 1, 1)
+        buttons['b'] = _GUIButton(root, '↓', 2, 1)
+        buttons['l'] = _GUIButton(root, '←', 2, 0)
+        buttons['r'] = _GUIButton(root, '→', 2, 2)
+        buttons['c'] = _GUIButton(root, 'Crouch', 4, 0)
+        buttons['j'] = _GUIButton(root, 'Jump', 4, 2)
+        self.buttons = buttons
 
-        with dpg.window(label="SurfChan", width=window_width, height=window_height):
-            self.buttons = {
-                'f': _GUIButton('^', 0, 1),
-                'b': _GUIButton('v', 1, 1),
-                'l': _GUIButton('<', 1, 0),
-                'r': _GUIButton('>', 1, 2),
-                'c': _GUIButton('Crouch', 2, 0),
-                'j': _GUIButton('Jump', 2, 2),
-            }
-
-        dpg.show_viewport()
+        self.update("")
 
     def update(self, button_str):
-        pressed_buttons = set(button_str)
+        pressed_buttons = list(button_str)
         for key, button in self.buttons.items():
             button.update(key in pressed_buttons)
         
@@ -66,5 +37,4 @@ class SCGUI:
 
 if __name__ == "__main__":
     gui = SCGUI()
-    dpg.start_dearpygui()
-    dpg.destroy_context()
+    gui.root.mainloop()
